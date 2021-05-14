59496a1e487817ac4cc45023a8a9c33e6539925f
Itamar-Gozlan <itamar.goz@gmail.com>
2021-05-14 16:08:28 +0300

    Updated XV6-RISCV code for procfs assignment

---
+++ kernel/procfs.d
@@ -0,0 +1,4 @@
+kernel/procfs.o: kernel/procfs.c kernel/types.h kernel/stat.h \
+ kernel/riscv.h kernel/defs.h kernel/param.h kernel/spinlock.h \
+ kernel/sleeplock.h kernel/fs.h kernel/file.h kernel/memlayout.h \
+ kernel/proc.h
